0a80130ecb6737f4639af430cd7cf7c81d2833c9
Gabriel Saldana <gabriel@gabrielsaldana.org>
2016-08-18 21:00:54 -0500

    Fixes and simplifies views code

--- sabesqmc/quote/views.py
+++ sabesqmc/quote/views.py
@@ -1,30 +1,48 @@
 from django.shortcuts import render, redirect
 from django.views.generic import DetailView, ListView
+from django.views.generic.base import RedirectView
 
 # Create your views here.
 from .models import Quote
 from .forms import QuoteForm
 
-def random(request):
+
+class RandomQuoteView(RedirectView):
     """
     Shows a random message
     """
-    quote = Quote()
-    random_quote = quote.get_random_msg()
-    redirect('message', args=[random_quote.id])
+    pattern_name = 'quote'
 
-class MessageView(DetailView):
+    def get_redirect_url(self, *args, **kwargs):
+        quote = Quote.objects.get_random()
+        return super(RandomQuoteView, self).get_redirect_url(quote.pk, **kwargs)
+
+class QuoteDetailView(DetailView):
     context_object_name = 'quote'
-    queryset = Quote.objects.all()
+    template_name = 'detail.html'
+    model = Quote
 
 
 class MostVotedView(ListView):
+    model = Quote
     queryset = Quote.objects.approved().order_by('-votes')
     context_object_name = 'quote_list'
+    template_name = 'list.html'
     paginate_by = 10
 
+    def get_context_data(self, **kwargs):
+        context = super(MostVotedView, self).get_context_data(**kwargs)
+        context['list_type'] = 'votadas'
+        return context
 
 class RecentView(ListView):
+    model = Quote
     queryset = Quote.objects.approved().order_by('-date_created')
     context_object_name = 'quote_list'
+    template_name = 'list.html'
     paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super(RecentView, self).get_context_data(**kwargs)
+        context['list_type'] = 'recientes'
+        return context
