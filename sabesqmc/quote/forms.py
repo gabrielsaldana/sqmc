d1763fd46d374cc09c865a05d8352b419b60b904
Gabriel Saldana <gabriel@gabrielsaldana.org>
2016-08-30 14:58:09 -0500

    Fixes form and adds more validations

--- sabesqmc/quote/forms.py
+++ sabesqmc/quote/forms.py
@@ -5,32 +5,43 @@ import re
 from django import forms
 from django.core import validators
 from django.db import IntegrityError
+from django.forms import ModelForm
 
 from .models import Quote
 
-class QuoteForm(forms.Form):
+class QuoteForm(ModelForm):
     """
     Form for sending a new message
     """
-    quote = forms.CharField(max_length=140, initial='Me caga ')
-    is_annonymous = forms.BooleanField(required=False,
-                                       widget=forms.CheckboxInput())
+    message = forms.CharField(label='Desahógate aquí', max_length=140, initial='Me caga ')
+    # is_annonymous = forms.BooleanField(required=False,
+    #                                    widget=forms.CheckboxInput())
 
-    def clean(self):
+    class Meta:
+        model = Quote
+        fields = ['message',]
+
+    def clean_message(self):
         """
         Checks for a valid message
         """
-        quote = self.cleaned_data['quote']
+        quote = self.cleaned_data['message']
         if not quote:
-            raise forms.ValidationError("No puedes enviar un mensaje vacío.")
+            raise forms.ValidationError("Me caga que intentes enviar un mensaje vacío.")
         # check for quotes without the primary words
         phrase = re.compile(r'me\s+caga', re.I)
         if not phrase.match(quote):
-            raise forms.ValidationError("Mensaje inválido. Sólo dinos qué te caga")
+            raise forms.ValidationError("Mensaje inválido. Sólo dinos qué te caga.")
         # check for short or empty quote
         if quote.lower() == 'me caga' or len(quote) <= 7:
             raise forms.ValidationError("Tu mensaje es demasiado corto.")
         # check for long quotes
         if len(quote) > 140:
             raise forms.ValidationError("Tu mensaje esta demasiado largo. Dinos qué te caga en 140 caracteres.")
-        return self.cleaned_data
+        # check for urls
+        if re.search(r'(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?', quote):
+            raise forms.ValidationError("A mi me caga que pongas URLs en tu mensaje.")
+        # check for emails
+        if re.search(r'([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})', quote):
+            raise forms.ValidationError('Me caga que pongas emails en tu mensaje.')
+        return self.cleaned_data['message']
