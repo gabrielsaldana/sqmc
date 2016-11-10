3fe2e2c41c14903eb6b7d685e24caa7089b12b11
Gabriel Saldana <gabriel@gabrielsaldana.org>
2016-11-10 17:45:51 -0600

    Fixes bower components and customized Bootstrap

--- sabesqmc/gulpfile.js
+++ sabesqmc/gulpfile.js
@@ -6,7 +6,10 @@ require('es6-promise').polyfill();
 
 var gulp = require('gulp'),
   pjson = require('./package.json'),
+  mainBowerFiles = require("main-bower-files"),
+  inject = require('gulp-inject'),
   sass = require('gulp-sass'),
+  customizeBootstrap = require('gulp-customize-bootstrap'),
   autoprefixer = require('gulp-autoprefixer'),
   cssnano = require('gulp-cssnano'),
   rename = require('gulp-rename'),
@@ -40,19 +43,28 @@ var paths = pathsConfig();
 ////////////////////////////////
 
 // Styles autoprefixing and minification
-gulp.task('styles', function() {
-  return gulp.src(paths.sass + '/styles.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(plumber()) // Checks for errors
-    .pipe(autoprefixer({
-      browsers: ['last 2 version']
-    })) // Adds vendor prefixes
-    .pipe(pixrem()) // add fallbacks for rem units
-    .pipe(gulp.dest(paths.css))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(cssnano()) // Minifies the result
+// gulp.task('styles', function() {
+//   return gulp.src('./bower_components/bootstrap/scss/bootstrap.scss')
+//     .pipe(customizeBootstrap(paths.sass + '/*.scss'))
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(plumber()) // Checks for errors
+//     .pipe(autoprefixer({
+//       browsers: ['last 2 version']
+//     })) // Adds vendor prefixes
+//     .pipe(pixrem()) // add fallbacks for rem units
+//     .pipe(gulp.dest(paths.css));
+//     // .pipe(rename({
+//     //   suffix: '.min'
+//     // }))
+//     // .pipe(cssnano()) // Minifies the result
+//     // .pipe(gulp.dest(paths.css));
+// });
+
+gulp.task('styles', function () {
+  return gulp.src(paths.sass + '/*.scss')
+    .pipe(sass({
+      includePaths:['./static/bower_components/bootstrap/scss/']
+    }).on('error', sass.logError))
     .pipe(gulp.dest(paths.css));
 });
 
@@ -67,6 +79,14 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest(paths.js));
 });
 
+gulp.task('html', function(){
+  return gulp.src(paths.templates + '/*.html')
+    .pipe(inject(gulp.src(
+      mainBowerFiles(), {read: false}),{name: 'bower'}))
+    .pipe(inject(gulp.src(paths.css + '/*.css', {read: false})))
+    .pipe(gulp.dest(paths.templates));
+});
+
 // Run django server
 gulp.task('runServer', function() {
   exec('python manage.py runserver', function(err, stdout, stderr) {
@@ -78,14 +98,14 @@ gulp.task('runServer', function() {
 // Browser sync server for live reload
 gulp.task('browserSync', function() {
   browserSync.init(
-    [paths.css + '/*.css', paths.js + '*.js', paths.templates + '*.html'], {
+    [paths.css + '/*.css', paths.js + '/*.js', paths.templates + '/*.html'], {
       proxy: 'localhost:8000'
     });
 });
 
 // Default task
 gulp.task('default', function() {
-  runSequence(['styles'], 'runServer', 'browserSync');
+  runSequence(['html', 'styles'], 'runServer', 'browserSync');
 });
 
 ////////////////////////////////
@@ -97,6 +117,5 @@ gulp.task('watch', ['default'], function() {
   gulp.watch(paths.sass + '/*.scss', ['styles']);
   gulp.watch(paths.js + '/*.js', ['scripts']).on('change', reload);
   gulp.watch(paths.images + '/*', ['imgCompression']);
-  gulp.watch(paths.templates + '/*.html').on('change', reload);
-  gulp.watch(paths.templates + '/**/*.html').on('change', reload);
+  gulp.watch(paths.templates + '/*.html', ['html']).on('change', reload);
 });
